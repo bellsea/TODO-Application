0f5c87a4b280a36ecdd54cfc5122c0cefda8cf39
satokaito <sato_kaito@plise.co.jp>
2025-01-21 15:34:17 +0900

    EntityとRepository

---
+++ Servers/todo/src/main/java/jp/ac/todo/Entity/User.java
@@ -0,0 +1,31 @@
+package jp.ac.todo.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "mailaddress", nullable = false, unique = true)
+    private String mailaddress;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
