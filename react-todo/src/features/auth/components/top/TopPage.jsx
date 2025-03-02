aa628b5cea10417bb0dc1f367803827f3c61ae5b
satokaito <sato_kaito@plise.co.jp>
2025-03-02 18:49:42 +0900

    画面大枠完成

--- react-todo/src/features/auth/components/top/TopPage.jsx
+++ react-todo/src/features/auth/components/top/TopPage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoCalendar from "../../../../components/calendar/TodoCalendar";
+import TodoList from "../../../../components/todolist/TodoList";
+import Layout from "../../../../components/layout/Layout";
+import "./TopPage.css";
 
 function TopPage() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Top Page</h1>
-      <p>これは仮のトップページです。</p>
-    </div>
+    <Layout title={"カレンダー"}>
+      <div className="calendar-container">
+        <TodoCalendar onDateSelect={setSelectedDate} />
+      </div>
+      <div className="todolist-container">
+        <TodoList selectedDate={selectedDate} />
+      </div>
+    </Layout>
   );
 }
 
