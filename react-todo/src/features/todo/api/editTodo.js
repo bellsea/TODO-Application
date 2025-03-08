37fed34ff825c68f9c415362c3b6dad809589c23
鈴木友也 <suzy_tomy@docomo.ne.jp>
2025-03-08 18:07:41 +0900

    todo編集画面、API呼び出し実装

---
+++ react-todo/src/features/todo/api/editTodo.js
@@ -0,0 +1,24 @@
+import axiosClient from "../../../lib/axiosClient";
+
+/**
+ * TODO取得API
+ */
+export const getTodo = () => {
+    return axiosClient.get("/todo/edit");
+}
+
+/**
+ * TODO追加API
+ */
+export const editTodo = (title, spanDate, spanTime, explain, isRoutine, routine) => {
+  const params = {
+    title,
+    spanDate,
+    spanTime,
+    explain,
+    isRoutine,
+    routine
+  };
+
+  return axiosClient.post("/todo/edit", params);
+};
